b1f1632dcc442be41900324064bb965113f74d89
Hugo Rochelle <113190509+HugoooR@users.noreply.github.com>
2025-04-12 14:26:02 +0200

    Update product.model.ts

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,8 +1,8 @@
 export interface Product {
-  id?: number;
+  productID?: number;
   name: string;
   description: string;
   price: number;
-  stock: number;
+  unitsInStock: number;
   imageUrl?: string;
 }
